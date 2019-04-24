ea508c114428d18c84f2a085a853aaab4aad959a
sahmed17 <sayfyas25@gmail.com>
2019-04-24 10:50:29 -0400

    took out api key

--- CuseHacksExtension/location.js
+++ CuseHacksExtension/location.js
@@ -11,7 +11,7 @@ navigator.geolocation.getCurrentPosition(function (position)
     lon = position.coords.longitude;
     $.getJSON
     (
-        "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=a9a989dd961c88bcd744adf275742f05",function(json)
+        "http://api.openweathermap.org/data/2.5/weather?lat=" + lat + "&lon=" + lon + "&APPID=apikey",function(json)
         {
         console.log(json['main']['temp']);
         var temp = Math.round(9/5 * (json ['main']['temp'] - 273) + 32);
